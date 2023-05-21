8b68b75a81666544471ad958b1e2a441262d227e
danny-mak1993 <danny.mak2@gmail.com>
2023-05-21 15:47:06 -0500

    changed login/signup code to identify issues

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -15,18 +15,21 @@ const checkUserInactivity = (req, res, next) => {
 
 // Dashboard homepage
 router.get('/', withAuth, async (req, res) => {
+    console.log("test1");
     try {
-
+        console.log(req.session,"req.session");
+        console.log("test2");
         req.session.lastActivity = Date.now();
         // Retrieve blogs created by the logged-in user
         const blogData = await Blog.findAll({
             where: { user_id: req.session.user_id },
         });
-
+        console.log("test3");
         // Serialize data so the template can read it
         const blogs = blogData.map((blog) => blog.get({ plain: true }));
+        console.log(blogs);
 
-        res.render('dashboard', { blogs, loggedIn: true });
+        res.render('dashboard', { blogs });
     } catch (err) {
         res.status(500).json(err);
     }
