c8ab1f654293e83c1289853363c082405b06071a
danny-mak1993 <danny.mak2@gmail.com>
2023-05-20 22:01:36 -0500

    changing routing code for getting individual blogs

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -28,28 +28,6 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
-router.get('/:id', async (req, res) => {
-    try {
-        const blogData = await Blog.findByPk(req.params.id, {
-            include: [
-                { model: User },
-                { model: Comment, include: [{ model: User }] }
-            ]
-        });
-
-        if (!blogData) {
-            res.status(404).json({ message: 'No blog found with this id' });
-            return;
-        }
-
-        const blog = blogData.get({ plain: true });
-        res.render('blog-details', { blog }); // Render the 'blog-details' view with the blog data
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-
 router.delete('/:id', withAuth, async (req, res) => {
     try {
         req.session.lastActivity = Date.now();
