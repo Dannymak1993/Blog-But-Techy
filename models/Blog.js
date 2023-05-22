128fd1f5228371681c51158ac87945ab9374e580
danny-mak1993 <danny.mak2@gmail.com>
2023-05-22 17:21:59 -0500

    added new sample blogs

--- models/Blog.js
+++ models/Blog.js
@@ -16,7 +16,7 @@ Blog.init(
             allowNull: false,
         },
         description: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
         },
         created_at: {
             type: DataTypes.DATE,
